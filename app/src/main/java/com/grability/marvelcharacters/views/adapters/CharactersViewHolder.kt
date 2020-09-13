6a8624f255c54353adff8ce6982e893980c3647c
John <jmurillo@sevenminds.com>
2020-09-13 11:43:05 -0500

    Add character adapter and view holder for recycler view items

---
+++ app/src/main/java/com/grability/marvelcharacters/views/adapters/CharactersViewHolder.kt
@@ -0,0 +1,45 @@
+package com.grability.marvelcharacters.views.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.grability.marvelcharacters.R
+import com.grability.marvelcharacters.data.model.ResultsModel
+import com.squareup.picasso.Picasso
+
+class CharacterViewHolder(inflater: LayoutInflater, parent: ViewGroup) : RecyclerView.ViewHolder(inflater.inflate(R.layout.item_character, parent, false)) {
+
+    private var context: Context? = null
+    var imageView: ImageView? = null
+    var description: TextView? = null
+    var title: TextView? = null
+
+    init {
+        context = parent.context
+        imageView = itemView.findViewById(R.id.iconIv)
+        description = itemView.findViewById(R.id.subtitle_txt)
+        title = itemView.findViewById(R.id.title_txt)
+    }
+
+    fun bind(results: ResultsModel) {
+        Picasso.with(context).isLoggingEnabled = true
+        Picasso.with(context)
+            .load(results.thumbnailModel?.path?.replace("http://","https://") + "/landscape_incredible." + results.thumbnailModel?.extension)
+            .into(imageView)
+
+        title?.text = results.name
+
+        if(results.description != null && !results.description.equals(""))
+            description?.text = results.description
+        else
+            description?.visibility = View.GONE
+
+        itemView.setOnClickListener {
+
+        }
+    }
+}

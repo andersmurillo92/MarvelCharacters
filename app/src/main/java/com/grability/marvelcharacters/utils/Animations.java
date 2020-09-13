b2d391c19d1293df4fba129966306abda01a31de
John <jmurillo@sevenminds.com>
2020-09-13 11:43:34 -0500

    Implement class to manage recycler view item animations and its anim resources

---
+++ app/src/main/java/com/grability/marvelcharacters/utils/Animations.java
@@ -0,0 +1,28 @@
+package com.grability.marvelcharacters.utils;
+
+import android.content.Context;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.grability.marvelcharacters.R;
+
+public class Animations {
+
+    public Animations() {
+        super();
+    }
+
+    public void runRecyclerAnimation(RecyclerView recyclerView) {
+
+        Context context = recyclerView.getContext();
+        AnimationUtils.loadLayoutAnimation(context, R.anim.recycler_item_anim);
+        LayoutAnimationController controller;
+
+        controller = AnimationUtils.loadLayoutAnimation(context, R.anim.recycler_item_anim);
+
+        recyclerView.setLayoutAnimation(controller);
+        recyclerView.getAdapter().notifyDataSetChanged();
+        recyclerView.scheduleLayoutAnimation();
+    }
+}

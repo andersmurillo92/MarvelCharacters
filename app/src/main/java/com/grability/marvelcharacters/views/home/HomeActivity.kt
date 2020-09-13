a99f4067e1bedf1e8669fc7f65890a10e2f5cd47
John <jmurillo@sevenminds.com>
2020-09-13 07:32:50 -0500

    Create home activity and its layout resource

---
+++ app/src/main/java/com/grability/marvelcharacters/views/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.grability.marvelcharacters.views.home
+
+import android.os.Bundle
+import com.grability.marvelcharacters.R
+import com.grability.marvelcharacters.views.base.BaseActivity
+
+class HomeActivity: BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}

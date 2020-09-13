4dcf40336fc20102ce3d431582f3682b6f21151f
John <jmurillo@sevenminds.com>
2020-09-12 22:39:04 -0500

    Add methods to detect network connection for BaseActivity

--- app/src/main/java/com/grability/marvelcharacters/views/base/BaseActivity.kt
+++ app/src/main/java/com/grability/marvelcharacters/views/base/BaseActivity.kt
@@ -2,9 +2,16 @@ package com.grability.marvelcharacters.views.base
 
 import android.app.Activity
 import android.app.ProgressDialog
+import android.content.Context
+import android.net.ConnectivityManager
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.grability.marvelcharacters.R
+import kotlinx.android.synthetic.main.layout_simple_toast.view.*
 
 open class BaseActivity: AppCompatActivity() {
 
@@ -26,5 +33,27 @@ open class BaseActivity: AppCompatActivity() {
     fun hideProgressDialog() {
         if(progressDialog.isShowing)
             progressDialog.dismiss()
+    }
+
+    fun showSimpleToast(content: String) {
+
+        val inflater = LayoutInflater.from(this@BaseActivity)
+        val layout = inflater.inflate(R.layout.layout_simple_toast, null)
+        layout.tv_ad.text = content
+
+        val mToast = Toast(this@BaseActivity)
+        mToast.setGravity(Gravity.CENTER, 0, 0)
+        mToast.duration = Toast.LENGTH_LONG
+        mToast.view = layout
+        mToast.show()
+    }
+
+    fun hasNetworkConnection(): Boolean {
+        var result = false
+        val connMgr = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connMgr.activeNetworkInfo
+        if (networkInfo != null && networkInfo.isConnected)
+            result = true
+        return result
     }
 }
